54cfcfd7e4a3c72a600c6b23f8d28048f1234441
Ethan <A02314504@aggies.usu.edu>
2023-06-07 11:17:03 -0600

    tetrahedron

--- Platonic-Rendering/scripts/driver.js
+++ Platonic-Rendering/scripts/driver.js
@@ -1,43 +1,88 @@
-//import { loadFileFromServer } from "./utilities";
 
 MySample.main = (function() {
     'use strict';
 
     let canvas = document.getElementById('canvas-main');
     let gl = canvas.getContext('webgl2');
+    gl.enable(gl.CULL_FACE);
+    gl.cullFace(gl.BACK);
 
-    let vertices = new Float32Array([
-        0.0, 0.5, 0.0,
-        0.5, 0.0, 0.0,
-        -0.5, 0.0, 0.0
+    const left =   -2;
+    const right =   2;
+    const top =     2;
+    const bottom = -2;
+    const near =    2;
+    const far =     10;
+
+    const camera = [0, 0, 2];
+
+    let r = 0.1;
+    // let rotateYZ = [
+    //     1, 0, 0, 0,
+    //     0, Math.cos(r), Math.sin(r), 0,
+    //     0, -Math.sin(r), Math.cos(r), 0,
+    //     0, 0, 0, 1,
+    // ];
+
+    const model = [];
+
+    const view =    [
+                        1, 0, 0, -camera[0],
+                        0, 1, 0, -camera[1],
+                        0, 0, 1, -camera[2],
+                        0, 0, 0, 1
+                    ];
+
+    const parallelProj =   [
+                                2 / (right - left), 0, 0, 0, 
+                                0, 2 / (top - bottom), 0, 0, 
+                                0, 0, -2 / (far - near),  0,
+                                -(left + right) / (right - left), -(top + bottom) / (top - bottom), -(far + near) / (far - near), 1
+                           ];
+
+    const perspectiveProj = [
+                                (2 * near) / (right - left), 0, 0, 0, 
+                                0, (2 * near) / (top - bottom), 0, 0, 
+                                (right + left) / (right - left), (top + bottom) / (top - bottom), -(far + near) / (far - near), -1, 
+                                0, 0, -2 * (far * near) / (far - near), 0
+                            ];
+
+    //Step 3: prepare raw data
+
+    //tetrahedron
+    let vertices1 = new Float32Array([
+        0.0, 1.0, 0.0,
+        1.0, 0.0, 0.0,
+        -1.0, 0.0, 0.0,
+        0.0, 0.5, 1.0,
     ]);
 
-    let indices = new Uint16Array([ 0, 1, 2 ]);
+    let indices1 = new Uint16Array([ 3, 1, 0, 3, 0, 2, 3, 2, 1 ]);
 
-    let vertexColors = new Float32Array([
+    let vertexColors1 = new Float32Array([
         1.0, 0.0, 0.0,
         0.0, 1.0, 0.0,
-        0.0, 0.0, 1.0
+        0.0, 0.0, 1.0,
+        1.0, 1.0, 0.0,
     ]);
 
+    //create buffers 
     let vertexBuffer = gl.createBuffer(); //create buffer object in webgl
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW); //put vertices data into buffer object 
+    gl.bufferData(gl.ARRAY_BUFFER, vertices1, gl.STATIC_DRAW); //put vertices data into buffer object 
     gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
     let vertexColorBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexColorBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, vertexColors, gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER, vertexColors1, gl.STATIC_DRAW);
     gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
     let indexBuffer = gl.createBuffer(); 
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices1, gl.STATIC_DRAW);
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
 
     //Step 5 : prepare shaders 
-        // string of the vertex shader code
-
     let vertexShader = null;
     let fragmentShader = null;
     let shaderProgram = null;
@@ -69,13 +114,13 @@ MySample.main = (function() {
             gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
             let position = gl.getAttribLocation(shaderProgram, 'aPosition');
             gl.enableVertexAttribArray(position);
-            gl.vertexAttribPointer(position, 3, gl.FLOAT, false, vertices.BYTES_PER_ELEMENT * 3, 0); //vertices.BYTES = stride 
+            gl.vertexAttribPointer(position, 3, gl.FLOAT, false, vertices1.BYTES_PER_ELEMENT * 3, 0); //vertices.BYTES = stride 
             gl.bindBuffer(gl.ARRAY_BUFFER, null); //unbind?
 
             gl.bindBuffer(gl.ARRAY_BUFFER, vertexColorBuffer);
             let color = gl.getAttribLocation(shaderProgram, 'aColor');
             gl.enableVertexAttribArray(color);
-            gl.vertexAttribPointer(color, 3, gl.FLOAT, false, vertexColors.BYTES_PER_ELEMENT * 3, 0);
+            gl.vertexAttribPointer(color, 3, gl.FLOAT, false, vertexColors1.BYTES_PER_ELEMENT * 3, 0);
             gl.bindBuffer(gl.ARRAY_BUFFER, null); //unbind?
 
             requestAnimationFrame(animationLoop);
@@ -84,49 +129,35 @@ MySample.main = (function() {
             console.log("something went wrong: ");
             console.log(error);
         });
-        
-    // let vertexShader = gl.createShader(gl.VERTEX_SHADER);
-    // gl.shaderSource(vertexShader, vertexShaderSource);
-    // gl.compileShader(vertexShader);
-    // console.log(gl.getShaderInfoLog(vertexShader)); // for debugging
 
-    // let fragmentShaderSource = loadFileFromServer('shaders/fragment.frag'); // string of the fragment shader code
-    // let fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-    // gl.shaderSource(fragmentShader, fragmentShaderSource);
-    // gl.compileShader(fragmentShader);
-
-    //create shader program
-    //attach fragment and vertex shader to shader program 
-        // let shaderProgram = gl.createProgram();
-        // gl.attachShader(shaderProgram, vertexShader);
-        // gl.attachShader(shaderProgram, fragmentShader);
-        // gl.linkProgram(shaderProgram);
-        // gl.useProgram(shaderProgram); //set webgl state to use this shader program 
-
-    //Step 6 : Specify Shader & Buffer Object Attributes
-    // gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-    // let position = gl.getAttribLocation(shaderProgram, 'aPosition');
-    // gl.enableVertexAttribArray(position);
-    // gl.vertexAttribPointer(position, 3, gl.FLOAT, false, vertices.BYTES_PER_ELEMENT * 3, 0); //vertices.BYTES = stride 
-    // //gl.bindBuffer(gl.ARRAY_BUFFER, null);
-
-    // gl.bindBuffer(gl.ARRAY_BUFFER, vertexColorBuffer);
-    // let color = gl.getAttribLocation(shaderProgram, 'aColor');
-    // gl.enableVertexAttribArray(color);
-    // gl.vertexAttribPointer(color, 3, gl.FLOAT, false, vertexColors.BYTES_PER_ELEMENT * 3, 0);
-    // //gl.bindBuffer(gl.ARRAY_BUFFER, null);
-
-        
+ 
     //------------------------------------------------------------------
     //
     // Scene updates go here.
     //
     //------------------------------------------------------------------
+ 
     function update() {
         // webGl uniform parameters
-        // let uMatrix = [...];
-        // let location = gl.getUniformLocation(shader, 'uThing');
-        // gl.uniformMatrix4fv(location, false, uMatrix);
+        if(shaderProgram == null){
+            return;
+        }
+
+        let rotateYZ = [
+            1, 0, 0, 0,
+            0, Math.cos(r), Math.sin(r), 0,
+            0, -Math.sin(r), Math.cos(r), 0,
+            0, 0, 0, 1,
+        ];
+       
+        let locationM = gl.getUniformLocation(shaderProgram, 'model');
+        let locationV = gl.getUniformLocation(shaderProgram, 'view');
+        let locationP = gl.getUniformLocation(shaderProgram, 'projection');
+        gl.uniformMatrix4fv(locationM, false, transposeMatrix4x4(rotateYZ));
+        gl.uniformMatrix4fv(locationV, false, transposeMatrix4x4(view));
+        gl.uniformMatrix4fv(locationP, false, transposeMatrix4x4(parallelProj));
+
+        r += 0.02;
     }
 
     //------------------------------------------------------------------
@@ -135,7 +166,7 @@ MySample.main = (function() {
     //
     //------------------------------------------------------------------
     function render() {
-        if(shaderProgram == undefined){
+        if(shaderProgram == null){
             return;
         }
 
@@ -151,7 +182,7 @@ MySample.main = (function() {
 
         //step 9
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-        gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
+        gl.drawElements(gl.TRIANGLES, indices1.length, gl.UNSIGNED_SHORT, 0);
     }
 
     //------------------------------------------------------------------
--- Platonic-Rendering/shaders/vertex.vert
+++ Platonic-Rendering/shaders/vertex.vert
@@ -4,8 +4,12 @@ in vec4 aPosition;
 in vec4 aColor;
 out vec4 vColor;
 
+uniform mat4 model; 
+uniform mat4 view; 
+uniform mat4 projection; 
+
 void main()
 {
-    gl_Position = aPosition;
+    gl_Position = projection * view * model * aPosition;
     vColor = aColor;
 }
